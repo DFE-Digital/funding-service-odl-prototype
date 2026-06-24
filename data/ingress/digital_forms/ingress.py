9f8aef745137e329744b6e3c07a5dc6d57275c37
benmartin-png <ben.martin@hippodigital.co.uk>
2026-06-24 16:46:50 +0100

    Added polars import and added code

--- data/ingress/digital_forms/ingress.py
+++ data/ingress/digital_forms/ingress.py
@@ -1,8 +1,12 @@
 import requests
 import pandas as pd
+import polars as pl
 
 def do_something():
     return pd.DataFrame()
 
 if __name__ == '__main__':
-    print(do_something())
+    print(do_something.__dict__)
+    print(vars(do_something))
+
+print()
