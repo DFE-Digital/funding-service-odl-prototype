86edc21c6bd31fe5a62ca1845fac8ca9b657f925
benmartin-png <ben.martin@hippodigital.co.uk>
2026-06-25 14:42:24 +0100

    Committing starting point Python code for the 4 APIs

---
+++ data/ingress/digital_forms/listFormConfigurations.py
@@ -0,0 +1,12 @@
+import requests
+
+url = "https://preprod.externalapi.digital-forms.education.gov.uk/api/listFormConfigurations"
+
+payload = {}
+headers = {
+  'Cookie': 'BNIS___utm_is1=iOhL/yEZovIFKNeArS2Ynm5E3krDMFhhvawqqwn2BZrMdhp74duH9QnEwKgLqRDD2jp24zCpTEnersbm4bjSqMyArkI+wBxiHhuvWFW6NI9n1If+pzqsaQ==; BNIS___utm_is2=EFgCQgAaEXLw3hLzBng+v7njvN2G4iIoK/xrcILLEN+g1qm3fc0KZl09WWDch//pR2i8825+pJ4=; BNIS___utm_is3=suvQyLhFSyq+S/jbW+6/0rIkJkC7MiJY4aaGmwjfZ0WIjM90rJEYcAIsnJQevrfkQI0J3asCYACYu6pY1ExffF5yqOKiuYQl0JOeIzZ3wuk=; BNIS_vid=Y8C70dUv6TGhd5aLcDn3BOsAXv50SeMUvz/pmhbyZidvnKTMmQ2Xz21a+yyIXrdcTNx5HiChFu1kjEcapg5ztb8CEUF9V8VNKVUttfAaVns='
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
