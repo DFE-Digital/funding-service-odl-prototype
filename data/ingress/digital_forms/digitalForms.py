729d0cd60f86dc8dee893305b9ebb9fc3a6bba0f
benmartin-png <ben.martin@hippodigital.co.uk>
2026-06-29 11:34:20 +0100

    Made line length and docstring formatting changes.

--- data/ingress/digital_forms/digitalForms.py
+++ data/ingress/digital_forms/digitalForms.py
@@ -5,6 +5,8 @@ from getResponseQuestion import url as getResponseQuestion_url
 from getResponseQuestionData import url as getResponseQuestionData_url
 
 class GetDF:
+    """This class can be instantiated with the required arguments to filter
+    data across the 4 Digital Forms APIs."""
     def __init__(self,start_date,end_date,form_id):
 
         self.start_date = start_date
@@ -18,16 +20,33 @@ class GetDF:
 
     
     def get(self,*APIs):
-        API_options = ['listFormConfigurations','getResponses','getResponseQuestion','getResponseQuestionData']
+        """This instance method can be called with a subset or all 4 Digital
+        Forms APIs, using the arbitrary positional arguments parameter (*APIs)
+        to retrieve the data"""
+        API_options = ['listFormConfigurations','getResponses',
+                       'getResponseQuestion','getResponseQuestionData']
         for submit in APIs:
             if submit not in API_options:
-                raise ValueError(f"Unsupported API name. Enter one or more of the following: {API_options}")
+                raise ValueError(f"Unsupported API name. Enter" \
+                                 "one or more of the following: {API_options}")
         
-        cookie = 'BNIS___utm_is1=iOhL/yEZovIFKNeArS2Ynm5E3krDMFhhvawqqwn2BZrMdhp74duH9QnEwKgLqRDD2jp24zCpTEnersbm4bjSqMyArkI+wBxiHhuvWFW6NI9n1If+pzqsaQ==; BNIS___utm_is2=EFgCQgAaEXLw3hLzBng+v7njvN2G4iIoK/xrcILLEN+g1qm3fc0KZl09WWDch//pR2i8825+pJ4=; BNIS___utm_is3=suvQyLhFSyq+S/jbW+6/0rIkJkC7MiJY4aaGmwjfZ0WIjM90rJEYcAIsnJQevrfkQI0J3asCYACYu6pY1ExffF5yqOKiuYQl0JOeIzZ3wuk=; BNIS_vid=Y8C70dUv6TGhd5aLcDn3BOsAXv50SeMUvz/pmhbyZidvnKTMmQ2Xz21a+yyIXrdcTNx5HiChFu1kjEcapg5ztb8CEUF9V8VNKVUttfAaVns='
+        cookie = 'BNIS___utm_is1=iOhL/yEZovIFKNeArS2Ynm5E3krDMFhhvawqqwn2BZr' \
+        'Mdhp74duH9QnEwKgLqRDD2jp24zCpTEnersbm4bjSqMyArkI+wBxiHhuvWFW6NI9n1I' \
+        'f+pzqsaQ==; BNIS___utm_is2=EFgCQgAaEXLw3hLzBng+v7njvN2G4iIoK/xrcILL' \
+        'EN+g1qm3fc0KZl09WWDch//pR2i8825+pJ4=; BNIS___utm_is3=suvQyLhFSyq+S/' \
+        'jbW+6/0rIkJkC7MiJY4aaGmwjfZ0WIjM90rJEYcAIsnJQevrfkQI0J3asCYACYu6pY1' \
+        'ExffF5yqOKiuYQl0JOeIzZ3wuk=; BNIS_vid=Y8C70dUv6TGhd5aLcDn3BOsAXv50S' \
+        'eMUvz/pmhbyZidvnKTMmQ2Xz21a+yyIXrdcTNx5HiChFu1kjEcapg5ztb8CEUF9V8VN' \
+        'KVUttfAaVns='
         payload = {}
 
-        headers_structure = dict(zip(API_options,[self.listFormConfigurations_pattern,self.getResponses_pattern,self.getResponseQuestion_pattern,self.getResponseQuestionData_pattern]))
-        headers_arguments = dict(zip(['StartDate','FormId','EndDate','Cookie'],[self.start_date,self.formid,self.enddate,cookie]))
+        headers_structure = dict(zip(API_options,[self.listFormConfigurations_pattern,
+                                                  self.getResponses_pattern,
+                                                  self.getResponseQuestion_pattern,
+                                                  self.getResponseQuestionData_pattern]))
+        headers_arguments = dict(zip(['StartDate','FormId','EndDate','Cookie'],
+                                     [self.start_date,self.formid,self.enddate,
+                                      cookie]))
 
         headers_structures = []
         #becomes a list of lists
@@ -37,10 +56,14 @@ class GetDF:
         #print(headers_structures)
         
         arguments = []
-        #becomes a list of lists with the actual instantiated header arguments for each API submitted
-        #it will be in the order that the APIs were submitted, not the API_options order
+        #becomes a list of lists with the actual instantiated header arguments
+        #for each API submitted
+        #it will be in the order that the APIs were submitted, not the
+        #API_options order
         #for instance:
-        #[['Cookie'], ['StartDate', 'FormId', 'EndDate', 'Cookie'], ['StartDate', 'FormId', 'EndDate', 'Cookie'], ['StartDate', 'FormId', 'EndDate']]
+        #[['Cookie'], ['StartDate', 'FormId', 'EndDate', 'Cookie'],
+        # ['StartDate', 'FormId', 'EndDate', 'Cookie'],
+        # ['StartDate', 'FormId', 'EndDate']]
         for i in range(0,len(headers_structures)):
             arguments.append([])
             for argument in headers_structures[i]:
@@ -48,7 +71,8 @@ class GetDF:
 
         #print(arguments)
 
-        #Now need to make dictionaries where headers_structures are the keys and arguments are the values
+        #Now need to make dictionaries where headers_structures are the keys
+        #and arguments are the values
         headers_list = [dict(zip(k, v)) for k, v in zip(headers_structures, arguments)]
         #print(headers_list)
 
@@ -67,7 +91,9 @@ class GetDF:
         
         response_data = []
         for i in range(0,len(APIs)):
-            response_data.append(requests.request("GET", URLs[i], headers=headers_list[i], data=payload).text)
+            response_data.append(requests.request("GET", URLs[i],
+                                                  headers=headers_list[i],
+                                                  data=payload).text)
 
 
   
@@ -75,4 +101,5 @@ class GetDF:
     
  
 instance = GetDF('2026-05-05','2026-06-20','7c6iy7ajyi')
-print(instance.get('listFormConfigurations','getResponses','getResponseQuestion','getResponseQuestionData'))
+print(instance.get('listFormConfigurations','getResponses',
+                   'getResponseQuestion','getResponseQuestionData'))
