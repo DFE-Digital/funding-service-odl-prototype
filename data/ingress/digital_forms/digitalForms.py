39781e0a02d5eab99f224548434e4fea3e81c84e
benmartin-png <ben.martin@hippodigital.co.uk>
2026-07-14 20:14:32 +0100

    Added more helper functions to prevent duplicated code.

--- data/ingress/digital_forms/digitalForms.py
+++ data/ingress/digital_forms/digitalForms.py
@@ -30,13 +30,15 @@ BASE_URL = "https://preprod.externalapi.digital-forms.education.gov.uk/api/"
 class DigitalForms:
     """This class can be instantiated with the API headers. Data from the 4
     APIs can be retrieved by calling an instance of this class."""
-    def __init__(self, start_date: str, end_date: str, form_ids: list[str]):
+    def __init__(self, start_date: str, end_date: str, form_ids: list[str],
+                 time_out: int = 300):
         """Initialises DigitalForms with both parameterised attributes and
         a session opening default-initialised attribute. Note that multiple
         form_ids can be passed."""
         self.start_date = start_date
         self.end_date = end_date
         self.form_ids = form_ids
+        self.time_out = time_out
         self.session = requests.Session()
         self.api_data = None
         self.api_data_json = None
@@ -51,6 +53,28 @@ class DigitalForms:
                 'EndDate': self.end_date,
                 'FormId': formid}
 
+    def data_check(self, endpoint, form_id, response):
+        """Logs based on the responses status code."""
+        check_prefix = f"form {form_id} and API {endpoint}" if form_id else \
+            f"API {endpoint}"
+        if response.status_code == 204:
+            logger.warning(f"No data found for {check_prefix} (code: "
+                           f"{response.status_code}).")
+        else:
+            logger.info(
+                f"Data found for {check_prefix} (code: {response.status_code}"
+                "). "
+                f"Time: {response.elapsed.total_seconds():.3f}s"
+                )
+
+    def validate_json(self, api_data, context):
+        """JSON validation that terminates ingestion if it fails."""
+        try:
+            json.loads(api_data)
+        except json.JSONDecodeError:
+            logger.error(f"Invalid JSON for {context}")
+            raise
+
     def get_data(self) -> dict:
         """Returns Digital Forms data from the 4 APIs."""
         logger.info("Downloading Digital Forms data...")
@@ -60,68 +84,39 @@ class DigitalForms:
         try:
             endpoint = 'listFormConfigurations'
             url = self.get_endpoint_url(endpoint)
-            api_response = self.session.get(url, timeout=300)
+            api_response = self.session.get(url, timeout=self.time_out)
             api_data = api_response.text
 
             # JSON validity check
-            try:
-                json.loads(api_data)
-            except json.JSONDecodeError:
-                logger.error(
-                    f"Invalid JSON for API {endpoint} "
-                    f"(code: {api_response.status_code}).")
+            self.validate_json(api_data, f"API {endpoint} (code: "
+                               f"{api_response.status_code}).")
 
             api_data_json = api_response.json()
 
-            if api_response.status_code == 204:
-                logger.warning("No data found for API "
-                               f"{endpoint} (code: "
-                               f"{api_response.status_code}).")
-            else:
-                logger.info("Data found for API "
-                            f"{endpoint} (code: "
-                            f"{api_response.status_code}). "
-                            f"Time: "
-                            f"{api_response.elapsed
-                               .total_seconds():.3f}"
-                            "s")
+            self.data_check(endpoint, None, api_response)
 
             all_data[endpoint] = api_data
             all_data_json[endpoint] = api_data_json
 
             for form_id in self.form_ids:
-                for endpoint in endpoint_list:
+                for ep in endpoint_list:
                     headers = self.get_headers(form_id)
-                    urls = self.get_endpoint_url(endpoint)
+                    urls = self.get_endpoint_url(ep)
                     api_response = self.session.get(urls, headers=headers,
-                                                    timeout=300)
+                                                    timeout=self.time_out)
                     api_data = api_response.text
 
                     # JSON validity check
-                    try:
-                        json.loads(api_data)
-                    except json.JSONDecodeError:
-                        logger.error(
-                            f"Invalid JSON for form {form_id} and API "
-                            f"{endpoint} (code: {api_response.status_code}).")
+                    self.validate_json(api_data, f"form {form_id} and API "
+                                       f"{ep} (code: "
+                                       f"{api_response.status_code}).")
 
                     api_data_json = api_response.json()
 
-                    if api_response.status_code == 204:
-                        logger.warning("No data found for form "
-                                       f"{form_id} and API {endpoint} "
-                                       f"(code: {api_response.status_code}).")
-                    else:
-                        logger.info("Data found for form "
-                                    f"{form_id} and API {endpoint} "
-                                    f"(code: {api_response.status_code}). "
-                                    f"Time: "
-                                    f"{api_response.elapsed
-                                       .total_seconds():.3f}"
-                                    "s")
+                    self.data_check(ep, form_id, api_response)
 
-                    all_data[form_id + "|" + endpoint] = api_data
-                    all_data_json[form_id + "|" + endpoint] = api_data_json
+                    all_data[form_id + "|" + ep] = api_data
+                    all_data_json[form_id + "|" + ep] = api_data_json
 
             logger.info("Digital Forms data downloaded.")
 
@@ -143,8 +138,8 @@ class DigitalForms:
             for exc in excels:
                 with pd.ExcelWriter(exc) as writer:
                     for sheet_name, json_text in self.api_data.items():
-                        if sheet_name.rpartition("|")[2] != exc.\
-                         rpartition("_")[2].replace(".xlsx", ""):
+                        if sheet_name.split("|")[-1] != exc.\
+                         split("_")[-1].replace(".xlsx", ""):
                             continue
                         df = pd.json_normalize(json.loads(json_text))
                         df.to_excel(writer, sheet_name=sheet_name[:31],
@@ -165,7 +160,9 @@ class DigitalForms:
         try:
             for js in jsons:
                 with open(js, 'w', encoding='utf-8') as file:
-                    filtered_data = {k: v for k, v in self.api_data_json.items() if k.rpartition("|")[2] == js.rpartition("_")[2].replace(".json", "")}
+                    filtered_data = {k: v for k, v in self.api_data_json
+                                     .items() if k.split("|")[-1] == js
+                                     .split("_")[-1].replace(".json", "")}
                     json.dump(filtered_data, file, indent=4)
             logger.success(f"Data has been written to the {json_prefix} "
                            "JSON files.")
@@ -190,7 +187,7 @@ def run_process(write_excel=False, write_JSON=True) -> None:
     digi_forms = DigitalForms('2020-05-05', '2026-06-20', relevant_forms)
     digi_forms.get_data()
 
-    prefix = "DigitalForms"
+    prefix = "Digital_Forms"
 
     if write_excel is True:
         digi_forms.write_to_excel(prefix)
