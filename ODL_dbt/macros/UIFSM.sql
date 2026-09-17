72047522b383639638b09bb1b701241a5e06ae2f
Andy Carter <andy.carter@education.gov.uk>
2026-09-17 08:46:32 +0100

    Fix typo in uifsm funding argument

--- ODL_dbt/macros/UIFSM.sql
+++ ODL_dbt/macros/UIFSM.sql
@@ -1,6 +1,6 @@
-{% macro UIFSM(UIFSM_pupils, portion_of_fuding_year, school_days_per_year, funding_amount_per_pupil) %}
+{% macro UIFSM(UIFSM_pupils, portion_of_funding_year, school_days_per_year, funding_amount_per_pupil) %}
     -- Calculates UIFSM funding based on the number of UIFAM pupils, the portion of the funding year(prelimiary or residual calculation),
     -- the number of funded school days per year, and the funding amount per eligible pupil
-    ({{UIFSM_pupils}}) * {{ portion_of_fuding_year }} * {{ school_days_per_year }} * {{ funding_amount_per_pupil }}
+    ({{UIFSM_pupils}}) * {{ portion_of_funding_year }} * {{ school_days_per_year }} * {{ funding_amount_per_pupil }}
 {% endmacro %}
 
